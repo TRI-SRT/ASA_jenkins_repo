2ccdd893155e7da0774e6a60bd950327bd7cf76e
TRI-SRT <Sureshanand242@gmail.com>
2025-09-23 15:49:34 +0530

    Update install_plugin.groovy

--- jenkins-plugin-model/install_plugin.groovy
+++ jenkins-plugin-model/install_plugin.groovy
@@ -1,18 +1,29 @@
-import jenkins.model.Jenkins;
+import jenkins.model.Jenkins
 
-pm = Jenkins.instance.pluginManager
-uc = Jenkins.instance.updateCenter
+def jenkins = Jenkins.get()
+def pm = jenkins.pluginManager
+def uc = jenkins.updateCenter
 
-// Calls Plugin Catalog and Download All the Information that require
-pm.doCheckUpdatesServer()
+// Update the Update Center metadata
+uc.updateAllSites()
 
-// List of PlugIn with Dependencies
-["github", "mstest", "workflow-aggregator", "docker-build-publish"].each {
-  if (! pm.getPlugin(it)) {
-    deployment = uc.getPlugin(it).deploy(true)
-    deployment.get()
-  }
+// List of plugins with dependencies
+def plugins = ["github", "mstest", "workflow-aggregator", "docker-build-publish"]
+
+plugins.each { pluginName ->
+    if (!pm.getPlugin(pluginName)) {
+        def plugin = uc.getPlugin(pluginName)
+        if (plugin) {
+            def deployment = plugin.deploy(true) // true = include dependencies
+            deployment.get() // wait for install
+            println "Installed plugin: ${pluginName}"
+        } else {
+            println "Plugin not found in update center: ${pluginName}"
+        }
+    } else {
+        println "Plugin already installed: ${pluginName}"
+    }
 }
 
 // Restart Jenkins after installing plugins (optional)
-Jenkins.instance.restart()
+// jenkins.restart()
